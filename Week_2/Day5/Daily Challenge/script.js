471fcf64f97c920a6913fbcff5aff47254c5290d
Yona-Dav <yona.bohbot@gmail.com>
2021-10-17 10:24:05 +0300

    fixed

--- Week_2/Day5/Daily Challenge/script.js
+++ Week_2/Day5/Daily Challenge/script.js
@@ -5,24 +5,26 @@ let number = parseInt(prompt("Enter a number"));
 
 // In the song everytime a bottle falls we subtract the bottles by 1.
 
+let count=0;
 
 function bottleBeer(){
-    for (i=number; i>0 ; i--){
-        if (i===1 && (number-i+1)!==1){
+    for (i=number; i>0 ; i=i-count){
+        count++
+        if (i===1 && count!==1){
             console.log( "1 bottle of beer on the wall \n".repeat(2) + "1 bottle of beer" );
-            console.log(`Take ${number} down, pass them around`)
+            console.log(`Take ${count} down, pass them around`)
         }
-        else if (i===1 && (number-i+1)===1){
+        else if (i===1 && count===1){
             console.log( "1 bottle of beer on the wall \n".repeat(2) + "1 bottle of beer" );
             console.log(`Take 1 down, pass it around`)
-        }else if (i!==1  && (number-i+1)===1){
+        }else if (i!==1  && count===1){
             console.log(`${i} bottles of beer on the wall \n ${i} bottles of beer on the wall \n ${i} bottles of beer `);
             console.log(`Take 1 down, pass it around`)
         }else{
             console.log(`${i} bottles of beer on the wall \n ${i} bottles of beer on the wall \n ${i} bottles of beer `);
-            console.log(`Take ${number-i+1} down, pass them around`)
+            console.log(`Take ${count} down, pass them around`)
         }
-    }
+    }console.log("No more bottles of beer on the wall...")
 }
 
 bottleBeer()
