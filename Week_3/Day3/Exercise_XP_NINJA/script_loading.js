219dc3c5df9b6ba765e68a9d1278e0f352c0a998
Yona-Dav <yona.bohbot@gmail.com>
2021-10-20 00:16:21 +0300

    fixed

--- Week_3/Day3/Exercise_XP_NINJA/script_loading.js
+++ Week_3/Day3/Exercise_XP_NINJA/script_loading.js
@@ -5,12 +5,11 @@ let counter =0;
 setInterval(addPoint, 300);
 
 function addPoint() {
-    if (counter<text.length){
-        elem.innerHTML = text[counter];
+    if (counter<point.length){
+        word.innerHTML = point[counter];
         counter++;
     }else{
         { counter = 0; }
     }
         
 }
-
