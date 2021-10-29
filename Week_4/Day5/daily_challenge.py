c899aba57eae2b4745ba8e4e9fd63b65787923fe
Yona-Dav <yona.bohbot@gmail.com>
2021-10-29 15:47:03 +0300

    daily fixed

--- Week_4/Day5/daily_challenge.py
+++ Week_4/Day5/daily_challenge.py
@@ -1,13 +1,11 @@
 # Write a program that accepts a comma separated sequence of words as input and prints the words in a comma-separated sequence after sorting them alphabetically.
 
-sequence = input('Enter a comma separated sequence of words ')
-print(sequence)
-
+#sequence = input('Enter a comma separated sequence of words ')
+sequence = 'without,hello,bag,world'
 seq = sequence.split(',')
-for i in range(len(seq)):
-    if seq[i][0] != seq[i+1][0]:
-        if ord(seq[i][0]) > ord(seq[i+1][0]):
-            seq[i][0], seq[i+1][0] = seq[i+1][0], seq[i][0]
+print(','.join(sorted(seq)))
 
-print(seq)
 
+# Use List Comprehension
+result = ','.join(sorted((x for x in sequence.split(','))))
+print(result)
