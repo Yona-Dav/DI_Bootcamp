5da0dd5bfa3f3cd0f433c13b7150d50f0aa1582d
Yona-Dav <yona.bohbot@gmail.com>
2021-11-11 19:46:58 +0200

    ex added

---
+++ Week_6/Day5/Daily_Challenge/daily_challenge.py
@@ -0,0 +1,60 @@
+import requests
+import json
+import random
+import psycopg2
+from tabulate import tabulate
+
+
+# HOSTNAME = 'localhost'
+# USERNAME = 'postgres'
+# PASSWORD = 'yoshir34'
+# DATABASE = 'countries'
+# connect = psycopg2.connect(host=HOSTNAME, user=USERNAME, password=PASSWORD, dbname=DATABASE )
+# cursor = connect.cursor()
+
+
+# cursor.execute('''CREATE TABLE countries
+#          (ID serial PRIMARY KEY     NOT NULL,
+#          name   varchar(50)   NOT NULL,
+#          capital         varchar(50) ,
+#          flag text,
+#          subregion varchar(255),
+#          population bigint );''')
+
+# connect.commit()
+# connect.close()
+
+HOSTNAME = 'localhost'
+USERNAME = 'postgres'
+PASSWORD = 'yoshir34'
+DATABASE = 'countries'
+
+def get_ten_countries():
+    url = 'https://restcountries.com/v3.1/all'
+    data = requests.get(url)
+    data = data.json()
+    for i in range(10):
+        x = random.randint(0,len(data))
+        print(x)
+        name = data[x]['name']['common']
+        capital = data[x]['capital'][0]
+        flag = data[x]['flags']['png']
+        subregion = data[x]['subregion']
+        population = data[x]['population']
+
+        connection = psycopg2.connect(host=HOSTNAME, user=USERNAME, password=PASSWORD, dbname=DATABASE )
+        cursor = connection.cursor()
+        query = f"INSERT INTO countries (name, capital, flag, subregion, population) VALUES ('{name}','{capital}','{flag}','{subregion}','{population}');"
+        cursor.execute(query)
+        connection.commit()
+        connection.close()
+    
+
+    connection = psycopg2.connect(host=HOSTNAME, user=USERNAME, password=PASSWORD, dbname=DATABASE )
+    cursor = connection.cursor()
+    cursor.execute("select * from countries")
+    connection.commit()
+    results = cursor.fetchall()
+    print(tabulate(results, headers=['Name','Capital', 'Flag','Subregion', 'Population']))
+
+get_ten_countries()
---
+++ Week_6/Day5/Exercise_XP_GOLD/exercises.py
@@ -0,0 +1,119 @@
+# WITH DICTIONARY
+
+users_info = {
+    "leo_d": "1234",
+    "alex_32":"5895",
+    "sarah_v": "9005"
+}
+
+# logged_in = []
+# while True:
+#     user_request = input('Enter login or exit ')
+
+#     if user_request == "exit":
+#         break
+#     elif user_request=='login':
+#         username =input('Enter your username')
+#         password = input("Enter your password")
+#         if username in users_info.keys() and users_info[username]==password:
+#             print('you are now logged in')
+#             logged_in.append(username)
+#         else:
+#             signup = input('Would you like to sign up? ')
+#             if signup == 'no':
+#                 break
+#             else:
+#                 username =input('Enter your username')
+#                 while username in users_info:
+#                     username=input('Enter your username')
+#                 password =input("Enter your password")
+#                 users_info[username]=[password]
+
+# print(users_info)
+
+# WITH DATABASE
+
+import psycopg2
+from tabulate import tabulate
+
+# Create a new table in pgAdmin
+
+# HOSTNAME = 'localhost'
+# USERNAME = 'postgres'
+# PASSWORD = 'yoshir34'
+# DATABASE = 'authentification'
+# connect = psycopg2.connect(host=HOSTNAME, user=USERNAME, password=PASSWORD, dbname=DATABASE )
+# cursor = connect.cursor()
+
+# cursor.execute('''CREATE TABLE authentification
+#          (ID serial PRIMARY KEY     NOT NULL,
+#          username   varchar(50)   NOT NULL,
+#          password   varchar(20)     NOT NULL);''')
+
+# cursor.execute("insert into authentification(username, password) values ('Lea', '1234') ")
+# cursor.execute("insert into authentification(username, password) values ('Sarah', 'hey34') ")
+# cursor.execute("insert into authentification(username, password) values ('Bob', 'game27') ")
+
+# connect.commit()
+# connect.close()
+HOSTNAME = 'localhost'
+USERNAME = 'postgres'
+PASSWORD = 'yoshir34'
+DATABASE = 'authentification'
+
+
+# logged_in = []
+# while True:
+#     user_request = input('Enter login or exit ')
+#     if user_request == "exit":
+#         break
+#     elif user_request=='login':
+#         username = input('Enter your username ')
+#         password = input("Enter your password ")
+#         connect = psycopg2.connect(host=HOSTNAME, user=USERNAME, password=PASSWORD, dbname=DATABASE )
+#         cursor = connect.cursor()
+#         cursor.execute(f"(select * from authentification where username ='{username}' and password='{password}')")
+#         user = cursor.fetchall()
+#         connect.close()
+#         if len(user) != 0:
+#             print('you are now logged in')
+#             logged_in.append(username)
+#         else:
+#             signup = input('Would you like to sign up? ')
+#             if signup == 'no':
+#                 break
+#             else:
+#                 username =input('Enter your username ')
+#                 connect = psycopg2.connect(host=HOSTNAME, user=USERNAME, password=PASSWORD, dbname=DATABASE )
+#                 cursor = connect.cursor()
+#                 cursor.execute(f"(select username from authentification where username ='{username}')")
+#                 name = cursor.fetchall()
+#                 connect.close()
+#                 while len(name) != 0:
+#                     username=input('Enter your username ')
+#                     connect = psycopg2.connect(host=HOSTNAME, user=USERNAME, password=PASSWORD, dbname=DATABASE )
+#                     cursor = connect.cursor()
+#                     cursor.execute(f"(select username from authentification where username ='{username}')")
+#                     name = cursor.fetchall()
+#                     connect.close()
+#                 password =input("Enter your password ")
+#                 connect = psycopg2.connect(host=HOSTNAME, user=USERNAME, password=PASSWORD, dbname=DATABASE )
+#                 cursor = connect.cursor()
+#                 cursor.execute(f"insert into authentification(username, password) values ('{username}', '{password}') ")
+#                 connect.commit()
+#                 connect.close()
+
+
+# To have a 'list' of username that exits in the database
+def see_users():
+    connect = psycopg2.connect(host=HOSTNAME, user=USERNAME, password=PASSWORD, dbname=DATABASE )
+    cursor = connect.cursor()
+    cursor.execute("select username from authentification ")
+    connect.commit()
+    users = cursor.fetchall()
+    connect.close()
+    print(tabulate(users, headers=['Username']))
+
+see_users()
+
+ 
